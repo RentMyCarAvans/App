0d722bd006a027cfb42cf13a6a594e2d44f8cdc6
Roy Schrauwen <roy@royschrauwen.nl>
2022-12-14 14:57:01 +0100

    [12] Added OfferView to MainActivity

--- app/src/main/java/com/avans/rentmycar/MainActivity.kt
+++ app/src/main/java/com/avans/rentmycar/MainActivity.kt
@@ -2,10 +2,33 @@ package com.avans.rentmycar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.avans.rentmycar.adapter.OfferAdapter
+import com.avans.rentmycar.data.TestDatasources
+import com.avans.rentmycar.databinding.ActivityMainBinding
+import com.avans.rentmycar.ui.GlideImageLoader
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val offerAdapter by lazy {
+        OfferAdapter(GlideImageLoader(this))
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val recyclerView = binding.mainRecyclerView
+        recyclerView.adapter = offerAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        offerAdapter.setData(
+            TestDatasources().loadOffers()
+        )
+
     }
 }
