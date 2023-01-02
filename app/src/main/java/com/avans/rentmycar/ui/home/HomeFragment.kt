908a7dbf15a48b1f41eec5746b0369413778f10b
Roy Schrauwen <roy@royschrauwen.nl>
2023-01-02 21:52:00 +0100

    [38] Fixed Merge Conflicts

--- app/src/main/java/com/avans/rentmycar/ui/home/HomeFragment.kt
+++ app/src/main/java/com/avans/rentmycar/ui/home/HomeFragment.kt
@@ -33,6 +33,8 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        //show actionbar
+        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
