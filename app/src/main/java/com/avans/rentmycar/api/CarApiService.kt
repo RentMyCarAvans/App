51695570f5d80f70e6109ae86df8bfa4ac0aa500
aubreypolderman <92875359+aubreypolderman@users.noreply.github.com>
2023-01-08 16:07:42 +0100

    Fixed hardcoded userId with SessionManager. Also fixed carapiservice @parameter userid with userId.

--- app/src/main/java/com/avans/rentmycar/api/CarApiService.kt
+++ app/src/main/java/com/avans/rentmycar/api/CarApiService.kt
@@ -20,7 +20,7 @@ interface CarApiService {
 
     // Retrieve all cars, and if the (optional) userid is passed, then retrieve all cars by userid
     @GET("/api/v1/cars")
-    suspend fun getAllCarsByUserId(@Query("userid") userId:Int) : Response<CarResponse>
+    suspend fun getAllCarsByUserId(@Query("userId") userId:Int) : Response<CarResponse>
 
     @DELETE("/api/v1/cars/{id}")
     suspend fun deleteCarById(@Path("id") Id:Int): Response<CarResponse>
