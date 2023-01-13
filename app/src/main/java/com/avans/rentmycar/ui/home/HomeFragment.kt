aa5091d5d210636ba66b702c7de249d1dec0120a
Roy Schrauwen <roy@royschrauwen.nl>
2023-01-13 14:35:13 +0100

    [46] New OfferListFragment

--- app/src/main/java/com/avans/rentmycar/ui/home/HomeFragment.kt
+++ app/src/main/java/com/avans/rentmycar/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.avans.rentmycar.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,19 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.avans.rentmycar.R
-import com.avans.rentmycar.adapter.OfferAdapter
 import com.avans.rentmycar.databinding.FragmentHomeBinding
-import com.avans.rentmycar.utils.GlideImageLoader
 import com.avans.rentmycar.utils.SessionManager
 import com.avans.rentmycar.viewmodel.OfferViewModel
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.material.snackbar.Snackbar
+
 
 class HomeFragment : Fragment() {
 
@@ -29,12 +26,21 @@ class HomeFragment : Fragment() {
     private val viewModel: OfferViewModel by viewModels()
     private val bookingViewModel: BookingViewModel by viewModels()
 
+    fun checkIfFragmentAttached(operation: Context.() -> Unit) {
+        if (isAdded && context != null) {
+            operation(requireContext())
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         (activity as AppCompatActivity?)!!.supportActionBar!!.show()
+
+
+
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -42,86 +48,108 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+
+
+//        val videoFragment = OfferListFragment()
+//        childFragmentManager.beginTransaction().apply {
+//            add(R.id.parent_fragment_container, videoFragment)
+//            commit()
+//        }
+
+
+
+
         // Set the TopButton colors to the default when the fragment is loaded
         // TODO: Replace this with a MD3 component
-        binding.buttonHomeAvailablecars.setBackgroundColor(resources.getColor(R.color.blue_200))
-        binding.buttonHomeMybookings.setBackgroundColor(resources.getColor(R.color.blue_500))
+        binding.buttonHomeAvailablecars.setBackgroundColor(resources.getColor(com.avans.rentmycar.R.color.blue_200))
+        binding.buttonHomeMybookings.setBackgroundColor(resources.getColor(com.avans.rentmycar.R.color.blue_500))
 
         // Set the title of the actionbar
         // TODO: Can this be done in the activity? Or in the navigation graph?
         val bar = (activity as AppCompatActivity).supportActionBar
-        bar?.title = getString(R.string.offers_title)
+        bar?.title = getString(com.avans.rentmycar.R.string.offers_title)
     }
 
     override fun onStart() {
         super.onStart()
 
         // Get the id of the logged in user so we can use it to get the correct offers and bookings
-        // TODO: Move this to the BookingsFragment for it is not needed here now because we do not filter offers on userId
         val userId = SessionManager.getUserId(requireContext())
 
+        checkIfFragmentAttached {
+            val offerListFragment: OfferListFragment = OfferListFragment()
+            val ft: FragmentTransaction = childFragmentManager.beginTransaction()
+            ft.replace(R.id.parent_fragment_container, offerListFragment);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        }
+
         // Make all the items in the recyclerview clickable, so the user can click on an item and go to the detail page of the selected offer
-        val offerAdapter = OfferAdapter(GlideImageLoader(view?.context as AppCompatActivity)) { offer ->
+//        val offerAdapter = OfferAdapter(GlideImageLoader(view?.context as AppCompatActivity)) { offer ->
+//
+//
+//            val action = HomeFragmentDirections.actionHomeFragment2ToHomeDetailFragment2(
+//                offer.id
+//            )
+//
+//            findNavController().navigate(action)
+//        }
 
-            // TODO: Only pass the ID and let the DetailsFragment fetch the data from the Collection
-            val action = HomeFragmentDirections.actionHomeFragment2ToHomeDetailFragment2(
-                offer.id
-            )
+//        binding.recyclerviewHomeFragmentOffers.layoutManager =
+//            LinearLayoutManager(view?.context, RecyclerView.VERTICAL, false)
+//        binding.recyclerviewHomeFragmentOffers.adapter = offerAdapter
 
-            //TODO: Andere Fragment maken voor de Booking Details
-            findNavController().navigate(action)
-        }
-
-        binding.recyclerviewHomeFragmentOffers.layoutManager =
-            LinearLayoutManager(view?.context, RecyclerView.VERTICAL, false)
-        binding.recyclerviewHomeFragmentOffers.adapter = offerAdapter
-
-        val model = ViewModelProvider(requireActivity())[OfferViewModel::class.java]
-        model.offerCollection.observe(viewLifecycleOwner) {
-        Log.d("[Home] model.offerColl", it.toString())
-            offerAdapter.setData(it)
-            if (it.isEmpty()) {
-                Log.d("[Home]", "No offers found")
-                // TODO: Show a text message that there are no offers available. For now, just show a snackbar
-                view?.let { it1 ->
-                    Snackbar.make(it1, "No offers found", Snackbar.LENGTH_LONG).show()
-                }
-            }
-            binding.progressIndicatorHomeFragment.visibility = View.GONE
-        }
-
-
-        // TODO: This should be in the Bookings fragment when it has been coded
-        bookingViewModel.bookingCollection.observe(viewLifecycleOwner) {
-            Log.d("[Home]", "Bookings: $it")
-            val offersFromMyBookings = bookingViewModel.bookingCollection.value?.map { it.offer } ?: emptyList()
-            Log.d("[Home]", "Offers from    my bookings: $offersFromMyBookings")
-            // TODO: Convert to BookingAdapter
-            offerAdapter.setData(offersFromMyBookings)
-        }
+//        val model = ViewModelProvider(requireActivity())[OfferViewModel::class.java]
+//        model.offerCollection.observe(viewLifecycleOwner) {
+//        Log.d("[Home] model.offerColl", it.toString())
+//            offerAdapter.setData(it)
+//            if (it.isEmpty()) {
+//                Log.d("[Home]", "No offers found")
+//                // TODO: Show a text message that there are no offers available. For now, just show a snackbar
+//                view?.let { it1 ->
+//                    Snackbar.make(it1, "No offers found", Snackbar.LENGTH_LONG).show()
+//                }
+//            }
+//            binding.progressIndicatorHomeFragment.visibility = View.GONE
+//        }
 
 
-        // Filter options
-        binding.btnBottomSheetModal.setOnClickListener {
-            HomeBottomSheetDialogFragment().show(childFragmentManager, "HomeBottomSheetDialogFragment")
-        }
+//        // TODO: This should be in the Bookings fragment when it has been coded
+//        bookingViewModel.bookingCollection.observe(viewLifecycleOwner) {
+//            Log.d("[Home]", "Bookings: $it")
+//            val offersFromMyBookings = bookingViewModel.bookingCollection.value?.map { it.offer } ?: emptyList()
+//            Log.d("[Home]", "Offers from    my bookings: $offersFromMyBookings")
+//            // TODO: Convert to BookingAdapter
+//            offerAdapter.setData(offersFromMyBookings)
+//        }
+
+
+//        // Filter options
+//        binding.btnBottomSheetModal.setOnClickListener {
+//            HomeBottomSheetDialogFragment().show(childFragmentManager, "HomeBottomSheetDialogFragment")
+//        }
 
         // Topbar navigation button Available cars
         binding.buttonHomeAvailablecars.setOnClickListener {
-            binding.buttonHomeAvailablecars.setBackgroundColor(resources.getColor(R.color.blue_200))
-            binding.buttonHomeMybookings.setBackgroundColor(resources.getColor(R.color.blue_500))
-            model.getOffers()
+            binding.buttonHomeAvailablecars.setBackgroundColor(resources.getColor(com.avans.rentmycar.R.color.blue_200))
+            binding.buttonHomeMybookings.setBackgroundColor(resources.getColor(com.avans.rentmycar.R.color.blue_500))
+            // TODO: Replace the displaying fragment with the OfferListFragment
+//            model.getOffers()
         }
 
         // Topbar navigation button My bookings
         binding.buttonHomeMybookings.setOnClickListener {
-            binding.buttonHomeAvailablecars.setBackgroundColor(resources.getColor(R.color.blue_500))
-            binding.buttonHomeMybookings.setBackgroundColor(resources.getColor(R.color.blue_200))
+            binding.buttonHomeAvailablecars.setBackgroundColor(resources.getColor(com.avans.rentmycar.R.color.blue_500))
+            binding.buttonHomeMybookings.setBackgroundColor(resources.getColor(com.avans.rentmycar.R.color.blue_200))
             if (userId != null) {
                 bookingViewModel.getBookings(userId)
                 // TODO: Maybe create a new layout for My Bookings?
-                val offersFromMyBookings = viewModel.bookingsResult.value?.map { it.offer } ?: emptyList()
-                offerAdapter.setData(offersFromMyBookings)
+//                val offersFromMyBookings = viewModel.bookingsResult.value?.map { it.offer } ?: emptyList()
+//                offerAdapter.setData(offersFromMyBookings)
+                // TODO: Replace the displaying fragment with the BookingListFragment
+
             } else {
                 // TODO: Show a text message that there are no bookings available. For now, just show a snackbar
                 view?.let { it1 ->
@@ -140,10 +168,10 @@ class HomeFragment : Fragment() {
                     "[Home] SM Deviceloc",
                     "Device location has not been set yet, so we wait for that"
                 )
-                binding.progressIndicatorHomeFragment.visibility = View.VISIBLE
+//                binding.progressIndicatorHomeFragment.visibility = View.VISIBLE
             } else {
                 Log.i("[Home] SM Deviceloc", "Device location has been set, so we can start!")
-                model.getOffers()
+//                model.getOffers()
             }
         }
 
