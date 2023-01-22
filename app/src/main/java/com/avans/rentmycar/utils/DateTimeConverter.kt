e4ac708fb53fbe5aa1bbc99632c2e164551a3b77
Roy Schrauwen <roy@royschrauwen.nl>
2023-01-22 06:56:23 +0100

    [22] Refactored Date- and TimePickers

--- app/src/main/java/com/avans/rentmycar/utils/DateTimeConverter.kt
+++ app/src/main/java/com/avans/rentmycar/utils/DateTimeConverter.kt
@@ -2,6 +2,7 @@ package com.avans.rentmycar.utils
 
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.Calendar
 
 object DateTimeConverter {
         fun convertDatabaseDateTimeToReadableDateTime(dateTime: String): String {
@@ -17,4 +18,23 @@ object DateTimeConverter {
             val localDateTime = LocalDateTime.parse(dateTime, inputFormat)
             return localDateTime.format(outputFormat)
         }
+
+        fun formatCalendarToString(caledar: Calendar) : String{
+            val year = caledar.get(Calendar.YEAR)
+            val month = caledar.get(Calendar.MONTH) + 1
+            val day = caledar.get(Calendar.DAY_OF_MONTH)
+            val hour = caledar.get(Calendar.HOUR_OF_DAY)
+            val minute = caledar.get(Calendar.MINUTE)
+            return "$day-$month-$year $hour:$minute"
+        }
+
+    fun formatCalendarToDBString(calendar: Calendar) : String{
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH) + 1
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+        return "$year-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}T${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:00"
+    }
+
 }
